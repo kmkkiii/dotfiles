7bb09fc42fdf2350e5de9adeb3e346d21060d3ac
kmkkiii <rskkmkmk@gmail.com>
2024-06-07 23:49:12 +0900

    update wezterm.lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,9 +6,13 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.window_background_opacity = 0.8
+config.color_scheme = 'Catppuccin Mocha'
+
+config.window_background_opacity = 0.75
 config.macos_window_background_blur = 20
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
+
+config.font_size = 12.0
 
 return config
