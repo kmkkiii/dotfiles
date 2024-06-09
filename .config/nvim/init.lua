397f69218891a96aff13314a9b812cabc87845e0
kmkkiii <rskkmkmk@gmail.com>
2024-06-09 16:34:44 +0900

    chore: add keymap for leaving insert mode with 'jj'

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -910,3 +910,6 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+-- normal mode
+vim.keymap.set('i', 'jj', '<ESC>', { silent = true })
