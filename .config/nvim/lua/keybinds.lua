2e99d30f8931b91e6d6749ce8262389c551a84d3
kmkkiii <rskkmkmk@gmail.com>
2024-06-19 23:02:38 +0900

    chore: Add keybindings, lazy loading, and options to Neovim configuration

---
+++ .config/nvim/lua/keybinds.lua
@@ -0,0 +1,9 @@
+vim.keymap.set('n', ';', ':')
+vim.keymap.set('n', ':', ';')
+
+vim.keymap.set('n', 'ZZ', '<NOP>')
+vim.keymap.set('n', 'ZQ', '<NOP>')
+
+vim.api.nvim_set_var('mapleader', ' ')
+vim.api.nvim_set_var('maplocalleader', '_')
+
