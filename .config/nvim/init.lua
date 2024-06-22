969f0750fe6e2c825daabb47e18e270191835657
kmkkiii <rskkmkmk@gmail.com>
2024-06-22 21:55:12 +0900

    chore: Update Neovim configuration with lazy loading for plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,4 +2,38 @@ vim.loader.enable()
 
 require 'options'
 require 'keybinds'
-require 'extensions'
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  'rcarriga/nvim-notify',
+  config = function() require 'plugins.nvim-notify' end,
+}
+
+require("lazy").setup(plugins, {
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
