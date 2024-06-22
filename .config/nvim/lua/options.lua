44e2697b7ce883b62433b2f2145557b78d2992d5
kmkkiii <rskkmkmk@gmail.com>
2024-06-22 21:54:43 +0900

    chore: Update Neovim options and window settings

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,25 +1,15 @@
-vim.api.nvim_set_option('termguicolors', true)
-vim.api.nvim_set_option('scrolloff', 4)
-vim.api.nvim_set_option('ignorecase', true)
-vim.api.nvim_set_option('smartcase', true)
-vim.api.nvim_set_option('inccommand', 'split')
-vim.api.nvim_set_option('clipboard', 'unnamedplus')
+vim.api.nvim_set_option_value('termguicolors', true, {})
+vim.api.nvim_set_option_value('scrolloff', 4, {})
+vim.api.nvim_set_option_value('ignorecase', true, {})
+vim.api.nvim_set_option_value('smartcase', true, {})
+vim.api.nvim_set_option_value('inccommand', 'split', {})
+vim.api.nvim_set_option_value('clipboard', 'unnamedplus', {})
 -- Window options:
-vim.api.nvim_win_set_option(0, 'number', true)
-vim.api.nvim_win_set_option(0, 'cursorline', true)
-vim.api.nvim_win_set_option(0, 'signcolumn', 'yes:1')
-vim.api.nvim_win_set_option(0, 'wrap', false)
+vim.api.nvim_set_option_value('number', true, {})
+vim.api.nvim_set_option_value('cursorline', true, {})
+vim.api.nvim_set_option_value('signcolumn', 'yes:1', {})
+vim.api.nvim_set_option_value('wrap', false, {})
 -- Buffer options:
--- Autocommand
-vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
-  pattern = '*',
-
-  group = vim.api.nvim_create_augroup('buffer_set_options', {}),
-
-  callback = function()
-    vim.api.nvim_buf_set_option(0, 'expandtab', true)
-    vim.api.nvim_buf_set_option(0, 'tabstop', 2)
-    vim.api.nvim_buf_set_option(0, 'shiftwidth', 0)
-  end,
-})
-
+vim.api.nvim_set_option_value('expandtab', true, {})
+vim.api.nvim_set_option_value('tabstop', 2, {})
+vim.api.nvim_set_option_value('shiftwidth', 0, {})
