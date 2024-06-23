ce4b29d9ad2ded709599f552a974d64c0b4e0fc1
kmkkiii <rskkmkmk@gmail.com>
2024-06-23 10:57:09 +0900

    chore: Add lualine and nvim-web-devicons plugins for status line and icons in Neovim

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,12 @@
+vim.api.nvim_set_option_value('showmode', false, {})
+
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  event = "VimEnter",
+  config = function()
+    require("lualine").setup {
+      options = { theme = "solarized_dark" },
+    }
+  end,
+}
