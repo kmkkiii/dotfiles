bfdb4286977eb4e5228b4b464244ab6b9720d867
kmkkiii <rskkmkmk@gmail.com>
2024-07-08 08:45:05 +0900

    feat: Add Lua language server configuration for Neovim

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -9,10 +9,29 @@ return {
   config = function()
     require("mason").setup {}
 
+    local lsp = require 'lspconfig'
+
     require('mason-lspconfig').setup_handlers {
       function(server_name)
         require('lspconfig')[server_name].setup {
           capabilities = require('cmp_nvim_lsp').default_capabilities(),
+        }
+      end,
+      ['lua_ls'] = function()
+        lsp.lua_ls.setup {
+          settings = {
+            Lua = {
+              runtime = {
+                version = 'LuaJIT',
+              },
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  vim.env.VIMRUNTIME,
+                },
+              },
+            },
+          },
         }
       end,
     }
