790ceb8b3a4e6894653e83242d9e9df319264bc7
kmkkiii <rskkmkmk@gmail.com>
2024-07-15 19:30:02 +0900

    chore: Update keybinds.lua for Neovim configuration

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -4,5 +4,7 @@ vim.keymap.set('n', ':', ';')
 vim.keymap.set('n', 'ZZ', '<NOP>')
 vim.keymap.set('n', 'ZQ', '<NOP>')
 
+vim.keymap.set('i', 'jj', '<ESC>', { silent = true })
+
 vim.api.nvim_set_var('mapleader', ' ')
 vim.api.nvim_set_var('maplocalleader', '_')
